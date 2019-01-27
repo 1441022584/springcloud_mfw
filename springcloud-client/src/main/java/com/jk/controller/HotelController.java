e3ba414fee88f34fc9e2b400507269824c710df8
njc8018693 <15028456181@163.com>
2019-01-27 13:34:22 +0800

    n

---
+++ springcloud-client/src/main/java/com/jk/controller/HotelController.java
@@ -0,0 +1,30 @@
+package com.jk.controller;
+
+import com.jk.service.HotelService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+
+@Controller
+@RequestMapping("hotel")
+public class HotelController {
+
+    @Resource
+    private HotelService hotelService;
+
+    //酒店首页
+    @RequestMapping("toHotel")
+    public String toHotel(){
+
+        return "hotel";
+    }
+    //北京 10065
+    @RequestMapping("10065")
+    public String beijing(){
+
+        return "10065beijing";
+    }
+
+
+}
